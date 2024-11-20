2d9d56db7f6fd8f018b104f7e8c41924cba46915
kashyrskyy <andriy.kashyrskyy@minerva.kgi.edu>
2024-11-20 15:23:05 -0500

    Fix vite config for xlsx and deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/itest-dev/',
+  optimizeDeps: {
+    include: ['xlsx'], // Ensure xlsx is pre-bundled
+  },
 })
